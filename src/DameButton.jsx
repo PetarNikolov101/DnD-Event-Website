5cb53aa07bb95e2ec81b36b86fc4907358047d46
PetarNikolov101 <petarnikolov101@gmail.com>
2024-05-20 00:23:36 +0200

    changed the logic for the button

--- src/DameButton.jsx
+++ src/DameButton.jsx
@@ -11,10 +11,15 @@ export default function DameButton(){
         }else SetShowComponent(false)
     }
 
+    const img = "spooky-log-cabin-dark-forest.jpg";
+    const text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, rerum dolor unde nobis quae et debitis maxime odit vel molestiae in aperiam distinctio praesentium, sint delectus? Quos, sapiente? Culpa, alias.";
+    const inverted = true;
+
+
     return (
         <>
             <button className="btn btn-primary" id="dame-button" onClick={ToggleComponent}>Barrow of the Writhing Prince</button><br/>
-            {ShowComponent && <OneshotDetails></OneshotDetails>}
+            {ShowComponent && <OneshotDetails img={img} text={text} inverted = {inverted}></OneshotDetails>}
         </>
     )
 }
--- src/OneshotDetails.jsx
+++ src/OneshotDetails.jsx
@@ -1,10 +1,21 @@
-export default function OneshotDetails(){
-    return(
+
+export default function OneshotDetails({img, text, inverted}){
+    if(inverted){
+        return(
         <div className="desc" id="oneshot-details">
-            <br></br>
-            <p className="desc-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, rerum dolor unde nobis quae et debitis maxime odit vel molestiae in aperiam distinctio praesentium, sint delectus? Quos, sapiente? Culpa, alias.</p>
-            <img src="spooky-log-cabin-dark-forest.jpg" className="desc-img"></img>
+        <br></br>
+        <img src={img} className="desc-img"></img>
+        <p className="desc-text">{text}</p>
         </div>
-        
-    )
+        )
+    }else{
+        return(
+            <div className="desc" id="oneshot-details">
+            <br></br>
+            <p className="desc-text">{text}</p>
+            <img src={img} className="desc-img"></img>
+        </div>
+        )
+    }
+
 }
